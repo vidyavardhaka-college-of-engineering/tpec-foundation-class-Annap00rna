0ba33fe7f7c2201853095f9736c6be0e0b8f919c
Annap00rna <96465307+Annap00rna@users.noreply.github.com>
2024-07-06 10:42:19 +0530

    Update Program_1.c

--- Program_1.c
+++ Program_1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int check_character(char c) {
+    if (isupper(c)) {
+        return 1;
+    } else if (islower(c)) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+int main() {
+    char input_char;
+    
+    printf("Enter a character: ");
+    scanf("%c", &input_char);
+    
+    int result = check_character(input_char);
+    printf("%d\n", result);
+    
+    return 0;
+}
 
